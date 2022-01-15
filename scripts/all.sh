97f9cb0cbf57880ce0a505b1fca7091b938c20d0
John S. Urban <urbanjost@comcast.net>
2022-01-14 22:32:08 -0500

    update docs

--- scripts/all.sh
+++ scripts/all.sh
@@ -5,22 +5,20 @@
 export PATH=/usr/bin:/bin
 HERE=$(realpath $(dirname $0))
 BASE=$(dirname $HERE)
-export PATH="$HERE:$PATH"
-export PATH="$BASE/bin:$PATH"
 export INTRINSICS=$BASE/intrinsics
 export MANPATH=$BASE/man
 
 # also need fpm(1)
 export FPMPATH="$(dirname $(which fpm) )"
-export PATH="$PATH:$FPMPATH"
-
-export FPMPATH="$(dirname $(which fpm) )"
-export PATH="$PATH:$FPMPATH"
 
 DOCS=$BASE/docs
 MANDIR=$BASE/man
 
 cd $BASE
+
+export PATH="$HERE:$PATH"
+export PATH="$BASE/bin:$PATH"
+export PATH="$PATH:$FPMPATH"
 ################################################################################
 # PURGE
 cd $BASE ||exit
